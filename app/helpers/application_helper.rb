3a0f661d52d235f28a803961b427fd0679869aaf
jam21 <jam.m.husain@gmail.com>
2015-03-14 09:48:17 +0500

    Static pages is done

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def full_title page_title=''
+    if page_title.empty?
+      "Ruby on Rails Tutorial Sample App|Ruby on Rails Tutorial Sample App"
+    else
+    "#{page_title}|Ruby on Rails Tutorial Sample App"
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Blog::Application.routes.draw do
   get "static_pages/home"
 
   get "static_pages/help"
-
+  get 'static_pages/about'
   resources :microposts
 
 
@@ -60,7 +60,7 @@ Blog::Application.routes.draw do
 resources :articles
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-   root :to => 'welcome#index'
+   root :to => 'static_pages#home'
 
   # See how all your routes lay out with "rake routes"
 
