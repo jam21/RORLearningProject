07d5ef5ed107b43fdb4bcb6e7429f72cca86d700
jam21 <jam.m.husain@gmail.com>
2015-04-01 13:28:01 +0500

    Added signup page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Blog::Application.routes.draw do
+  get "user/new"
+
   root :to=>'static_pages#home'
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
+  get 'signup' => 'users#new'
   resources :microposts
 
 
