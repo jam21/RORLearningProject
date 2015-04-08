1ed9ec9631fe8ef02429145dee344a4b82d91083
jam21 <jam.m.husain@gmail.com>
2015-04-08 17:28:47 +0500

    Added sign up page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  attr_accessible :email, :name
+  attr_accessible :email, :name, :password, :password_confirmation
   has_secure_password
   validates :name,presence: true,length: {maximum:55}
   validates :email,presence: true,length: {maximum:255}
--- db/schema.rb
+++ db/schema.rb
@@ -28,11 +28,9 @@ ActiveRecord::Schema.define(:version => 20150406073904) do
   end
 
   create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
+    t.string "name"
+    t.string "email"
+    t.string "password_digest"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
