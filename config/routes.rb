485a954fd74aafbf0884101fcc622cc28e26296a
jam21 <jam.m.husain@gmail.com>
2015-04-13 15:57:14 +0500

    Login functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Blog::Application.routes.draw do
+  get "sessions/new"
+
   get "user/new"
 
   root :to=>'static_pages#home'
@@ -6,6 +8,9 @@ Blog::Application.routes.draw do
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup' => 'users#new'
+  get    'login'   => 'sessions#new'
+  post   'login'   => 'sessions#create'
+  delete 'logout'  => 'sessions#destroy'
   resources :microposts
 
 
